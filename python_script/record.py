c8b010a7b2ff626addf7297692ddefa71af99da5
toolkiz <ankit.ghosh@protonmail.com>
2024-02-07 18:25:40 +0100

    streaming and preprocessing in single script

--- python_script/record.py
+++ python_script/record.py
@@ -8,7 +8,26 @@ import pyaudio
 import numpy as np
 
 from datetime import datetime
+from scipy.fft import rfft, rfftfreq
 from reduct import Client, BucketSettings, QuotaType
+
+def calculate_amplitude(channel_data):
+    return np.max(np.abs(channel_data))
+
+def calculate_fundamental_frequency(channel_data, rate):
+    # Perform Fourier Transform
+    N = len(channel_data)
+    yf = rfft(channel_data)
+    xf = rfftfreq(N, 1 / rate)
+
+    # Find the peak frequency
+    idx = np.argmax(np.abs(yf))
+    return xf[idx]
+
+def calculate_energy(channel_data):
+    return np.sum(channel_data.astype(float)**2) / len(channel_data)
+
+bucket_name = 'test_3'
 
 async def main():
     # create the pyaudio instance
@@ -39,10 +58,11 @@ async def main():
                 print("Recording...")
                 while True:
                     sys.stdout.write(f'\r{time.strftime("%Y-%m-%d %H:%M:%S")}')
+                    ts = int(datetime.now().timestamp() * 1e6)
                     data = stream.read(int(info['defaultSampleRate']))
-                    await bucket.write(f"test_2", 
+                    await bucket.write(f"{bucket_name}", 
                                        data,
-                                       timestamp=int(datetime.now().timestamp() * 1e6),
+                                       timestamp=ts,
                                        content_length=len(data),
                                        labels={'sample_rate': int(info['defaultSampleRate']),
                                                'channels': int(info['maxInputChannels']),
@@ -51,6 +71,25 @@ async def main():
                     
                     audio_data = np.frombuffer(data, dtype=np.int16)
                     audio_data = audio_data.reshape(-1, int(info['maxInputChannels']))
+
+                    normalized_data = audio_data.astype(np.float32) / np.iinfo(np.int16).max  # Normalize entire array
+
+                    # Calculate values for all channels using vectorized functions
+                    blender_values = np.zeros((3, int(info['maxInputChannels'])))
+                    blender_values[0, :] = np.apply_along_axis(calculate_amplitude, 0, normalized_data)
+                    blender_values[1, :] = np.apply_along_axis(calculate_fundamental_frequency, 0, normalized_data, int(info['defaultSampleRate'])) / int(info['defaultSampleRate'])
+                    blender_values[2, :] = np.apply_along_axis(calculate_energy, 0, normalized_data)
+
+                    blender_data = blender_values.flatten().tobytes()
+                    await bucket.write(
+                        f'{bucket_name}_parameters',
+                        blender_data,
+                        timestamp=ts,
+                        # content_length=len(blender_values.flatten()),
+                        labels={'dtype': blender_values.dtype,
+                                'channels': int(info['maxInputChannels']),
+                                'parameters': 3}
+                    )   
 
             except KeyboardInterrupt:
                 print("Recording stopped by user")

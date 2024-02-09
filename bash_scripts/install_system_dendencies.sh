ca9c1677d681176d6e3f07ed752afb16a819c4c9
toolkiz <ankit.ghosh@protonmail.com>
2024-02-09 10:38:47 +0100

    adding the dependencies required to be installed for pyaudio to work

---
+++ bash_scripts/install_system_dendencies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Update package lists
+sudo apt-get update
+
+# Install portaudio19-dev
+sudo apt-get install -y portaudio19-dev
+
+# Install build-essential
+sudo apt-get install -y build-essential

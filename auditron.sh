5e91c4808bb79daecbf993a79d2b0b72f1c3bb10
toolkiz <ankit.ghosh@protonmail.com>
2024-02-09 12:05:57 +0100

    adding the bash script to start the auditron recording

--- auditron.sh
+++ auditron.sh
@@ -1,4 +1,7 @@
-# start the server
+#!/bin/bash
 
+# Run Docker container in the background
+docker run -d -p 8383:8383 -v ${PWD}/data:/data my_custom_image &
 
-# start the recording
+# Execute Python script in parallel
+python my_script.py

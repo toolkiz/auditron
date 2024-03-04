a18c7ccc86e7a1da81df8b60a56bb93104779b7a
toolkiz <ankit.ghosh@proton.me>
2024-03-04 09:27:47 +0100

    reader py with function definitions

--- python_script/reader.py
+++ python_script/reader.py
@@ -27,7 +27,7 @@ ap.add_argument("-bn", "--bucket_name", type=str, default='likora',
                 # required=True, 
                 help="bucket to read from")
 
-ap.add_argument("-en", "--entry_name", type=str, default='likoranpp_parameters',
+ap.add_argument("-en", "--entry_name", type=str, default='octopus_parameters',
                 # required=True, 
                 help="entry to read from")
 
@@ -42,6 +42,18 @@ ap.add_argument("-t1", "--stop_time", type=str, default='2024-02-14 14:48',#1708
 args = vars(ap.parse_args())
 
 def unix_microsecond_format(timestamp):
+    '''
+    Convert a timestamp string to Unix time in microseconds.
+
+    This function takes a timestamp string formatted as '%Y-%m-%d %H:%M' and 
+    converts it to the number of microseconds since the Unix epoch (Jan 1, 1970).
+
+    Parameters:
+    timestamp (str): A timestamp string in the format of '%Y-%m-%d %H:%M'.
+    
+    Returns:
+    int: The Unix time representation of the input timestamp in microseconds.
+    '''
 
     datetime_obj = datetime.strptime(timestamp, '%Y-%m-%d %H:%M')
     timestamp = datetime_obj.timestamp()
@@ -49,12 +61,27 @@ def unix_microsecond_format(timestamp):
 
     return microseconds
 
-async def main(bucket_name, start_ts, stop_ts):
+async def main(bucket_name, entry_name, start_ts, stop_ts):
+    '''
+    Asynchronously queries a ReductStore bucket and retrieves entries within a specified time range.
+
+    This function creates a ReductStore client, queries the specified bucket for entries with the given name that fall within the start and stop timestamps, and processes the cumulative data into a feature data array.
+
+    Parameters:
+    bucket_name (str): The name of the ReductStore bucket to query.
+    entry_name (str): The name of the entry to query within the bucket.
+    start_ts (str): The start timestamp in '%Y-%m-%d %H:%M' format.
+    stop_ts (str): The stop timestamp in '%Y-%m-%d %H:%M' format.
+
+    Returns:
+    np.ndarray: A numpy array containing the processed feature data from the queried records.
+    '''
+
     # Create a ReductStore client
     async with Client(args["ip_address"]) as client:
 
         # get the bucket
-        bucket = await client.get_bucket(args['bucket_name'])
+        bucket = await client.get_bucket(bucket_name)
 
         # creating the time in milisecond
         ts0 = unix_microsecond_format(start_ts)
@@ -62,7 +89,7 @@ async def main(bucket_name, start_ts, stop_ts):
 
         cumulative_data = []
 
-        async for record in bucket.query(bucket_name, start=ts0, stop=ts1, ttl=1000):
+        async for record in bucket.query(entry_name, start=ts0, stop=ts1, ttl=1000):
 
             data = await record.read_all()
             cumulative_data.append(data)
@@ -83,8 +110,5 @@ async def main(bucket_name, start_ts, stop_ts):
             return np.array(cumulative_data)
 
 if __name__ == "__main__":
-    array_object = asyncio.run(main(bucket_name=args['entry_name'], start_ts=args['start_time'], stop_ts=args['stop_time']))
-
+    array_object = asyncio.run(main(bucket_name=args['bucket_name'], entry_name=args['entry_name'], start_ts=args['start_time'], stop_ts=args['stop_time']))
     pickle.dump(array_object, open('blender_array.aud', 'wb'))
-
-    # print(array_object
